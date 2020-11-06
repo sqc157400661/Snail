eb7287be54ad0a6db54c0899fd2094e63c58217b
sqc <sqc157400661@163.com>
2020-11-06 14:54:23 +0800

    1

--- Go专题系列/code/Go语言性能分析/pprof/main.go
+++ Go专题系列/code/Go语言性能分析/pprof/main.go
@@ -9,28 +9,32 @@ import (
 // 创建map不指定容量
 func makeMap1() map[int]int {
 	mp := make(map[int]int)
-	for i:=0;i<100000;i++{
-		mp[i] = i
-	}
-	return mp
-}
-// 创建map指定容量
-func makeMap2() map[int]int {
-	mp := make(map[int]int,100000)
-	for i:=0;i<100000;i++{
+	for i := 0; i < 100000; i++ {
 		mp[i] = i
 	}
 	return mp
 }
 
-func test1(w http.ResponseWriter, r *http.Request){
+// 创建map指定容量
+func makeMap2() map[int]int {
+	mp := make(map[int]int, 100000)
+	for i := 0; i < 100000; i++ {
+		mp[i] = i
+	}
+	return mp
+}
+
+func test1(w http.ResponseWriter, r *http.Request) {
 	makeMap1()
 	makeMap2()
-	fmt.Println(1111)
+	var ii interface{}
+	ii = 1
+	ii = "32342"
+	fmt.Println(ii)
 }
 
 func main() {
 	// 路由配置
 	http.HandleFunc("/test1", test1)
-	_ =http.ListenAndServe("0.0.0.0:6061", nil)
+	_ = http.ListenAndServe("0.0.0.0:6061", nil)
 }
