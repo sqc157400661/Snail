6701150889fe8a7b904d289fe3d47c0c1d86d97a
sqc <sqc157400661@163.com>
2020-11-24 14:19:06 +0800

    面向面试的知识点03-Map

---
+++ Go基础系列/code/面向面试的知识点/03Map/demo1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	Name string
+}
+
+var list map[string]Student
+
+func main() {
+
+	list = make(map[string]Student)
+
+	student := Student{"Aceld"}
+
+	list["student"] = student
+	list["student"].Name = "LDB"
+
+	fmt.Println(list["student"])
+}
